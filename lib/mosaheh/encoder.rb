954f4f24e5d4a36bdc1026480e0e2c4e376d485e
Maher Sallam <maher@sallam.me>
2011-11-04 21:18:53 +0100

    Refactored the initializer
    This should also improve the preformance, as now we only
    calculate the utf_8 beginnings, the problem and the correct char once.

--- lib/mosaheh/encoder.rb
+++ lib/mosaheh/encoder.rb
@@ -1,11 +1,19 @@
 # encoding: UTF-8
 
 class Mosaheh::Encoder
-    
-  # Initialize the encoder.
+  
+  # Initialize the encoder
   def initialize
-  end
+    # UTF-8 bytes-sequences always begin with one of (0xD8 - 0xDB) for Arabic
+    @utf_8_beginning_chars = [*216..219].map(&:chr).join.force_encoding('cp1252')
 
+    # Misencoded sequences can be correctly re-encoded to utf-8, EXCEPT for one
+    # charecter with gets replaced with a space (ASCII for it: 32)!
+    @problem_char = 32.chr.force_encoding('cp1252')
+
+    # The correct replacement for the problem charecter 
+    @correct_char = 160.chr.force_encoding('cp1252')
+  end
   
   # Repairs Arabic (U+0600 - U+06FF) data
   # which has been misencoded from cp1252 to UTF-8
@@ -14,14 +22,9 @@ class Mosaheh::Encoder
   # @param [String] misencoded string
   # @return [String] correctly encoded utf-8 string
   def repair(str)
-
+    
     source, dest = str.clone, ""
     ec = Encoding::Converter.new('utf-8', 'cp1252')
-
-    utf_beginning_chars = [*216..219].map(&:chr).join.force_encoding('cp1252')
-    problem_char = 32.chr.force_encoding('cp1252')
-    correct_char = 160.chr.force_encoding('cp1252')
-
     until source.empty?
 
       if source[0] =~ %r{([\u0600-\u06FF])+}u
@@ -37,8 +40,8 @@ class Mosaheh::Encoder
         dest += c.force_encoding('cp1252')
       end
 
-      if state == :after_output && dest =~ /[#{utf_beginning_chars}]#{problem_char}$/
-        dest.gsub!(/#{problem_char}$/, correct_char)
+      if state == :after_output && dest =~ /[#{@utf_8_beginning_chars}]#{@problem_char}$/
+        dest.gsub!(/#{@problem_char}$/, @correct_char)
       end
     end
 
