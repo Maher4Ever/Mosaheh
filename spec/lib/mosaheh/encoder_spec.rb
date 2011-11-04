08ead05fb8fcf5d1c6d1ad8f74964a720ccbbfcf
Maher Sallam <maher@sallam.me>
2011-11-04 21:58:24 +0100

    Add a Guardfile and change the spec files to a better nesting

--- spec/lib/mosaheh/encoder_spec.rb
+++ spec/lib/mosaheh/encoder_spec.rb
@@ -1,5 +1,5 @@
 # encoding: UTF-8
-require File.expand_path('../../spec_helper', __FILE__)
+require File.expand_path('../../../spec_helper', __FILE__)
 
 describe Mosaheh::Encoder do
   let(:encoder) { Mosaheh::Encoder.new }
